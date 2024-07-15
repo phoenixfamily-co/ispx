6350fbf1d2e58cd0f75dbecc5940e0556d9f3957
mohammadmma <mohammadmma3004@gmail.com>
2024-07-15 17:08:43 +0330

    initializing
    fixed venv, packages, ...

--- IranianShiningPhoenix/settings.py
+++ IranianShiningPhoenix/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home.apps.HomeConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Slider(models.Model):
+    Image = models.ImageField()
+    Title = models.CharField(max_length=255)
+
+
+class CEO(models.Model):
+    Title = models.CharField(max_length=255)
+    Image = models.ImageField()
+    Description = models.TextField()
