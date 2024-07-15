c82a562802aa81b7291d1076f812dd00aaa272a2
mohammadmma <mohammadmma3004@gmail.com>
2024-07-15 22:47:53 +0330

    create contact and send email
    start the contact app, fixed the files(serializers, urls, views, settings), fixed the email setting. develop CREATE API View and customize perform_create method

--- IranianShiningPhoenix/settings.py
+++ IranianShiningPhoenix/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
+    'contact.apps.ContactConfig',
     'rest_framework'
 ]
 
@@ -134,3 +135,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mohammadmma3004@gmail.com'
+EMAIL_HOST_PASSWORD = 'bstr naro lqfe tjje'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+COMPANY_EMAIL = 'aslmohammad10@gmail.com'
