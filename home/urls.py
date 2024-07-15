c9131208760cbcacf4e554e29e8b706389a98287
mohammadmma <mohammadmma3004@gmail.com>
2024-07-15 21:17:24 +0330

    generic-base CRUD API views for home application
    define model, define serializers, develop views.py, fixed urls, and also makemigrations and migrate.

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from .views import WelcomeView
+from .views import WelcomeView, SliderListCreateAPIView, SliderRetrieveUpdateDestroyAPIView, CeoListCreateAPIView, \
+    CeoRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path("", WelcomeView.as_view()),
+    path("sliders/", SliderListCreateAPIView.as_view(), name='customer-list-create'),
+    path("ceo/", CeoListCreateAPIView.as_view(), name='ceo-list-create'),
+    path("sliders/<int:pk>/", SliderRetrieveUpdateDestroyAPIView.as_view(), name='customer-detail'),
+    path("ceo/<int:pk>/", CeoRetrieveUpdateDestroyAPIView.as_view(), name='ceo-detail'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,5 +1,28 @@
 from django.views.generic import TemplateView
+from rest_framework import generics
+from .models import Slider, CEO
+from .serializers import SliderSerializer, CeoSerializer
 
 
 class WelcomeView(TemplateView):
     template_name = "main.html"
+
+
+class SliderListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Slider.objects.all()
+    serializer_class = SliderSerializer
+
+
+class SliderRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Slider.objects.all()
+    serializer_class = SliderSerializer
+
+
+class CeoListCreateAPIView(generics.ListCreateAPIView):
+    queryset = CEO.objects.all()
+    serializer_class = CeoSerializer
+
+
+class CeoRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CEO.objects.all()
+    serializer_class = CeoSerializer
