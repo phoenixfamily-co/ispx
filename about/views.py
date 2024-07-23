e8812a80e8a8c5dd09406bb806037a43a647dc52
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-07-23 18:12:41 +0330

    home navbar button added

--- about/views.py
+++ about/views.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.template import loader
 from django.views.decorators.cache import cache_page
-from django.views.generic import TemplateView
 
 
 # Create your views here.
