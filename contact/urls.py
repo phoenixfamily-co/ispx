5120290d341ac03d637114410b25ff10574fc318
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-08-01 23:41:24 +0330

    mma merged

--- contact/urls.py
+++ contact/urls.py
@@ -1,5 +1,8 @@
+from django.conf.urls.static import static
 from django.urls import path, include
-from .views import ContactViewSet,contract_view
+
+from IranianShiningPhoenix import settings
+from .views import ContactViewSet, contract_view
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -7,9 +10,12 @@ router.register(prefix=r'', viewset=ContactViewSet)
 
 app_name = 'contract'
 
-
 urlpatterns = [
-    path('contact-us/', include(router.urls)),
+    path('api/', include(router.urls, 'contract')),
     path("", contract_view, name='view'),
 
 ]
+
+urlpatterns += router.urls
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
