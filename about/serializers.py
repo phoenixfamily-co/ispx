38ed2adf7123adb4d802e4c54aa8e56313091f53
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-03-01 07:59:07 +0330

    category services added

--- about/serializers.py
+++ about/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import About
+from .models import About, Certificate
 
 
 class AboutSerializer(serializers.ModelSerializer):
@@ -10,5 +10,5 @@ class AboutSerializer(serializers.ModelSerializer):
 
 class CertificateSerializer(serializers.ModelSerializer):
     class Meta:
-        model = About
+        model = Certificate
         fields = '__all__'
